4091c847e12531996c2034d9c2d99e13671a8b21
Nikolakito <nikolakitoferreiros@gmail.com>
2024-08-19 15:13:56 -0300

    Empece con el componente de la barra

--- components/search.tsx
+++ components/search.tsx
@@ -1,8 +1,33 @@
+import { Box, Typography, TextField} from "@mui/material";
 export default function Page() {
     return (
-      <div>
-        <h1>hola</h1>
-      </div>
+      <Box>
+        <Typography sx={{ 
+    height: 36,
+    color: '#313237',
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    fontSize: 28,
+    lineHeight: '36px',
+  }}> Novedades</Typography>
+  <TextField 
+  placeholder="Tu placeholder aquí"
+  sx={{ 
+    '&::placeholder': { 
+      color: '#ADB5BD', // Color del placeholder
+      opacity: 1, // Asegúrate de que sea completamente visible
+    },
+    input: {
+      color: '#313237', // Color del texto al escribir
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
+      fontSize: 28,
+      lineHeight: '36px',
+    }
+  }}
+/>
+
+      </Box>
     );
   }
   
