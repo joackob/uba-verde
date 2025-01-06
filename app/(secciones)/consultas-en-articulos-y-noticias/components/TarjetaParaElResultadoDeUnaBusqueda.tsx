6010fe93dec4c6e0aead9f01b349cd6998c01fcd
Joaquin Blanco <blancojoaquin77@gmail.com>
2025-01-06 00:01:48 -0300

    refactor: cambie el estilo de las tarjetas dedicadas a los resultados de busquedas

--- app/(secciones)/consultas-en-articulos-y-noticias/components/TarjetaParaElResultadoDeUnaBusqueda.tsx
+++ app/(secciones)/consultas-en-articulos-y-noticias/components/TarjetaParaElResultadoDeUnaBusqueda.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, Typography } from "@mui/material";
 
 const TarjetaResumen = ({
   titulo,
@@ -10,37 +10,34 @@ const TarjetaResumen = ({
   url: string;
 }) => {
   return (
-    <Card
+    <Box
       component={"a"}
       href={url
         .replace(/\.html$/, "")
         .replace(/\/server/, "")
         .replace(/\/app/, "")
         .replace(/\/content/, "")}
-      sx={{ display: "flex", backgroundColor: "transparent" }}
     >
-      <CardContent>
-        <Typography
-          style={{
-            fontFamily: "Roboto",
-            fontWeight: 500,
-            fontSize: "14px",
-            color: "#313237",
-          }}
-        >
-          {titulo}
-        </Typography>
-        <Typography
-          style={{
-            fontFamily: "Roboto",
-            fontWeight: 400,
-            fontSize: "12px",
-            color: "#313237",
-          }}
-          dangerouslySetInnerHTML={{ __html: `${descripcion} ...` }}
-        />
-      </CardContent>
-    </Card>
+      <Typography
+        style={{
+          fontFamily: "Roboto",
+          fontWeight: 500,
+          fontSize: "14px",
+          color: "#313237",
+        }}
+      >
+        {titulo}
+      </Typography>
+      <Typography
+        style={{
+          fontFamily: "Roboto",
+          fontWeight: 400,
+          fontSize: "12px",
+          color: "#313237",
+        }}
+        dangerouslySetInnerHTML={{ __html: `${descripcion} ...` }}
+      />
+    </Box>
   );
 };
 
